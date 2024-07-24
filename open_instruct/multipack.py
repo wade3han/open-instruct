bd3df278a7fbe95cdd175266afca973dbb26d845
Seungju Han <wade3han@gmail.com>
2024-07-24 23:20:27 +0900

    ?

--- open_instruct/multipack.py
+++ open_instruct/multipack.py
@@ -24,16 +24,18 @@ LOG = logging.getLogger(__name__)
 
 
 def get_dataset_lengths(dataset):
-    # helper util to calculate dataset lengths
-    if "length" in dataset.data.column_names:
-        lengths = np.array(dataset.data.column("length"))
-    elif "position_ids" in dataset.data.column_names:
-        position_ids = dataset.data.column("position_ids")
-        lengths = np.array([x[-1] + 1 for x in position_ids])
-    else:
-        input_ids = dataset.data.column("input_ids")
-        lengths = np.vectorize(len)(np.array(input_ids, dtype=object))
-        return lengths
+    input_ids = dataset["input_ids"]
+    lengths = [len(x) for x in input_ids]
+    # # helper util to calculate dataset lengths
+    # if "length" in dataset.data.column_names:
+    #     lengths = np.array(dataset.data.column("length"))
+    # elif "position_ids" in dataset.data.column_names:
+    #     position_ids = dataset.data.column("position_ids")
+    #     lengths = np.array([x[-1] + 1 for x in position_ids])
+    # else:
+    #     input_ids = dataset.data.column("input_ids")
+    #     lengths = np.vectorize(len)(np.array(input_ids, dtype=object))
+    #     return lengths
     return lengths
 
 
