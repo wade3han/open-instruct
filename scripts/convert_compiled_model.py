b380286f33104a195e378e7921f4c48247c8397c
Seungju Han <wade3han@gmail.com>
2024-07-24 11:18:33 +0900

    fix

--- scripts/convert_compiled_model.py
+++ scripts/convert_compiled_model.py
@@ -1,6 +1,7 @@
 import argparse
+import os
 
-from transformers import AutoModelForCausalLM
+import torch
 
 
 def remove_prefix(text, prefix):
@@ -15,17 +16,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     model_name = args.model_name
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    state_dict = model.state_dict()
-
-    import ipdb;
-    ipdb.set_trace();
-
-    # Remove prefix "_orig_mod." from the keys.
-    new_state_dict = {}
-    for key, value in state_dict.items():
-        new_key = remove_prefix(key, "_orig_mod.")
-        new_state_dict[new_key] = value
-
-    model.load_state_dict(new_state_dict)
-    model.save_pretrained(model_name)
+    # search *.bin file in model_name directory
+    for file in os.listdir(model_name):
+        if file.endswith(".bin"):
+            bin_path = os.path.join(model_name, file)
+            new_state_dict = {}
+            state_dict = torch.load(bin_path)
+            for key, value in state_dict.items():
+                new_key = remove_prefix(key, "_orig_mod.")
+                new_state_dict[new_key] = value
+            torch.save(new_state_dict, bin_path)
