9944a8f52f7d2efcaeefada5b54cd478fbf3c48c
Seungju Han <wade3han@gmail.com>
2024-08-04 10:16:06 +0900

    loo expts

--- gantry/llama2_loo/run_trains.sh
+++ gantry/llama2_loo/run_trains.sh
@@ -1,13 +1,13 @@
-# bash gantry/llama2_loo/train_all.sh
-bash gantry/llama2_loo/train_lmsyschat.sh
-bash gantry/llama2_loo/train_tulu2mix-code_alpaca.sh
+bash gantry/llama2_loo/train_all.sh
+#bash gantry/llama2_loo/train_lmsyschat.sh
+#bash gantry/llama2_loo/train_tulu2mix-code_alpaca.sh
 bash gantry/llama2_loo/train_tulu2mix-cot.sh
 bash gantry/llama2_loo/train_tulu2mix-flan_v2.sh
 bash gantry/llama2_loo/train_tulu2mix-gpt4_alpaca.sh
 bash gantry/llama2_loo/train_tulu2mix-oasst1.sh
 bash gantry/llama2_loo/train_tulu2mix-open_orca.sh
 bash gantry/llama2_loo/train_tulu2mix-science.sh
-bash gantry/llama2_loo/train_tulu2mix-sharegpt.sh
+#bash gantry/llama2_loo/train_tulu2mix-sharegpt.sh
 bash gantry/llama2_loo/train_tulu2mix-wizardlm.sh
 bash gantry/llama2_loo/train_ultrachat.sh
 bash gantry/llama2_loo/train_ultrainteract.sh
