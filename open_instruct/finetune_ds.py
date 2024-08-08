c66dbf65db53ee08b9e809553090679ba6d0ae89
Seungju Han <wade3han@gmail.com>
2024-08-08 16:14:43 +0900

    v2.9

--- open_instruct/finetune_ds.py
+++ open_instruct/finetune_ds.py
@@ -699,9 +699,9 @@ def main():
             "weight_decay": 0.0,
         },
     ]
-    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
-    # optimizer = deepspeed.ops.adam.FusedAdam(optimizer_grouped_parameters, lr=args.learning_rate,
-    #                                          weight_decay=0.0, )
+    # optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
+    optimizer = deepspeed.ops.adam.FusedAdam(optimizer_grouped_parameters, lr=args.learning_rate,
+                                             weight_decay=0.0, )
 
     # Scheduler and math around the number of training steps.
     num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
