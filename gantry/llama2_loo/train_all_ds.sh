7702b0055a34c5ee2064179ad1f851634d456242
Seungju Han <wade3han@gmail.com>
2024-08-08 16:25:46 +0900

    fix

--- gantry/llama2_loo/train_all_ds.sh
+++ gantry/llama2_loo/train_all_ds.sh
@@ -7,7 +7,7 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory,
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
-NAME=ds_no_fused_loo-v2_megamixv2_batch4_seq8192_sum_lr5e-5_wsd20_user_mask_a100_all
+NAME=ds_loo-v2_megamixv2_batch4_seq8192_sum_lr5e-5_wsd20_user_mask_a100_all
 
 gantry run --beaker-image seungjuh/open-instruct-public-240806-preview --venv base --name $NAME --cluster ai2/pluto-cirrascale --workspace ai2/safety --pip requirements.txt --gpus 4 --priority high --preemptible --env-secret WANDB_API_KEY=WANDB_API_KEY --env-secret HF_TOKEN=HUGGING_FACE_HUB_TOKEN --env WANDB_PROJECT=llama2-finetuning --env WANDB_ENTITY=seungjuhan3 --env WANDB_NAME=$NAME --env-secret OPENAI_API_KEY=openai_api_key --budget ai2/oe-adapt -- \
   deepspeed open_instruct/finetune_ds.py \
