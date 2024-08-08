84ec0be7fa86625fef16c93d77e6a665977574dc
Seungju Han <wade3han@gmail.com>
2024-08-08 13:18:30 +0900

    update

--- gantry/llama2_loo/train_all_acc.sh
+++ gantry/llama2_loo/train_all_acc.sh
@@ -10,7 +10,14 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 NAME=ds_loo-v2_megamixv2_batch4_seq8192_sum_lr5e-5_wsd20_user_mask_a100_all
 
 gantry run --beaker-image seungjuh/open-instruct-public-240806-preview --venv base --name $NAME --cluster ai2/pluto-cirrascale --workspace ai2/safety --pip requirements.txt --gpus 4 --priority high --preemptible --env-secret WANDB_API_KEY=WANDB_API_KEY --env-secret HF_TOKEN=HUGGING_FACE_HUB_TOKEN --env WANDB_PROJECT=llama2-finetuning --env WANDB_ENTITY=seungjuhan3 --env WANDB_NAME=$NAME --env-secret OPENAI_API_KEY=openai_api_key --budget ai2/oe-adapt -- \
-  deepspeed open_instruct/finetune_accelerate_compare.py \
+  accelerate launch \
+  --mixed_precision bf16 \
+  --num_machines 1 \
+  --num_processes $NUM_GPUS \
+  --use_deepspeed \
+  --main_process_port 2950 \
+  --deepspeed_config_file configs/ds_configs/stage2_accelerate.conf \
+  open_instruct/finetune_accelerate_compare.py \
   --use_multipack \
   --use_compile \
   --mask_users \
