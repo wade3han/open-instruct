9d6196fc942d1ec5370312d716cb2da2b1b5c827
Seungju Han <wade3han@gmail.com>
2024-08-08 14:17:31 +0900

    fix

--- open_instruct/finetune_ds.py
+++ open_instruct/finetune_ds.py
@@ -555,18 +555,18 @@ def main():
     TEST_DATASET_DIR = "/net/nfs.cirrascale/mosaic/seungjuh/open-instruct/datasets/"
     selected_validation_dataset_names = [
         "lmsyschat",
-        # "tulu2mix-code_alpaca",
-        # "tulu2mix-cot",
-        # "tulu2mix-flan_v2",
-        # "tulu2mix-gpt4_alpaca",
-        # "tulu2mix-oasst1",
-        # "tulu2mix-open_orca",
-        # "tulu2mix-science",
-        # "tulu2mix-sharegpt",
-        # "tulu2mix-wizardlm",
-        # "ultrachat",
-        # "ultrainteract",
-        # "wildchat-gpt-4-0125-preview",
+        "tulu2mix-code_alpaca",
+        "tulu2mix-cot",
+        "tulu2mix-flan_v2",
+        "tulu2mix-gpt4_alpaca",
+        "tulu2mix-oasst1",
+        "tulu2mix-open_orca",
+        "tulu2mix-science",
+        "tulu2mix-sharegpt",
+        "tulu2mix-wizardlm",
+        "ultrachat",
+        "ultrainteract",
+        "wildchat-gpt-4-0125-preview",
     ]
     lm_datasets_tests = []
     for dataset_name in selected_validation_dataset_names:
