e7562a0a3ff52d8740b267027c5f81acafd59d39
Seungju Han <wade3han@gmail.com>
2024-08-14 20:19:07 +0900

    fix: smoothing

--- open_instruct/gradient/finetune_pure_mixture_faster.py
+++ open_instruct/gradient/finetune_pure_mixture_faster.py
@@ -59,7 +59,7 @@ class CombinedDataLoader:
         current_mixture_weights = np.array(current_mixture_weights) * np.exp(new_mixture_weights_coeff)
         current_mixture_weights = \
             (1 - self.smoothing_factor) * current_mixture_weights * current_mixture_weights.sum() + \
-            self.smoothing_factor * np.ones_like(current_mixture_weights)  # smoothing
+            self.smoothing_factor * np.ones_like(current_mixture_weights) / len(current_mixture_weights)
         current_mixture_weights = current_mixture_weights.tolist()
         print(f"New mixture weights: {current_mixture_weights}")
         self.mixture_weights = current_mixture_weights
