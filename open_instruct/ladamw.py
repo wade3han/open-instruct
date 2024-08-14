flora, ladamw fix + gradient tracing (#7)
* fix: accumulation_steps

* flora

* fix

* galore

* fix

* fix

* fix: galore

* fix: flora device

* fix

* fix galore

* fix: flora

* fix

* starting gradient research

* fix

* fix

* fix

* remove deepspeed

* finetune without deepspeed

* fix

* fix

* fix

* fix

* pure

* fix

* using mixture weights

* store gradients

* fix

* fix

* debug

* retry

* fix

* projection

* retry

* fix

* fix

* update

* fix

* fix

* debug

* debug

* fix

* debugging trak

* fix

* fix

* fix?

* fix

* fix

* fix

* fix

* block_size

* fix

* test lora

* lora

* lora rank 256

* fix

* no checkpointing

* fix

* flora 2nd

open_instruct/ladamw.py
@@ -55,7 +55,7 @@ class LAdamW(Optimizer):
         if lr < 0.0:
             raise ValueError(f"Invalid learning rate: {lr} - should be >= 0.0")
         if not 0.0 <= betas[0] <= 1.0:
-            raise ValueError(f"Invalid beta parameter: {betas[0]} - should be in [0.0, 1.0]")
+            raise ValueError(f"Invalid beta parameter: {betas[0]} - should be in [0.0, 1.0)")
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError(f"Invalid beta parameter: {betas[1]} - should be in [0.0, 1.0)")
         if not 0.0 <= betas[2] < 1.0:
@@ -74,6 +74,7 @@ class LAdamW(Optimizer):
 
         self.state['projection'] = torch.randn(max_size * rank)
         self.state['beta0'] = betas[0]
+        self.state['step'] = 0
         # self.state['hyperparams'] = (lr, rank, betas[0], betas[1], betas[2], eps, weight_decay, correct_bias)
 
     @torch.no_grad()
@@ -94,6 +95,8 @@ class LAdamW(Optimizer):
 
         projection.mul_(beta0).add_(torch.randn_like(projection), alpha=math.sqrt(1.0 - beta0 ** 2))
 
+        self.state['step'] += 1
+
         for group in self.param_groups:
             for p in group["params"]:
                 if p.grad is None:
@@ -103,8 +106,6 @@ class LAdamW(Optimizer):
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
                 state = self.state[p]
-                if "step" not in state:
-                    state["step"] = 0
 
                 if projection.device != p.device:
                     self.state['projection'] = projection.to(p.device)
@@ -114,17 +115,14 @@ class LAdamW(Optimizer):
 
                 # Compression
                 if p.dim() < 2:
-                    grad = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]) / math.sqrt(rank), grad)
-                    # grad = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]), grad)
+                    grad = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]) / math.sqrt(rank), grad)  # shape: [rank]
                 elif p.dim() == 2:
                     if p.shape[0] >= p.shape[1]:
                         grad = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]) / math.sqrt(rank),
                                             grad)
-                        # grad = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]), grad)
                     else:
                         grad = torch.matmul(grad,
                                             projection[:rank * p.shape[1]].view(p.shape[1], rank) / math.sqrt(rank))
-                        # grad = torch.matmul(grad, projection[:rank * p.shape[1]].view(p.shape[1], rank))
                 else:
                     raise ValueError("Parameters that exceed 2 Dim are not supported currently.")
 
@@ -136,7 +134,6 @@ class LAdamW(Optimizer):
                     state["exp_avg_sq"] = torch.zeros_like(grad)
 
                 exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
-                state["step"] += 1
 
                 # Decay the first and second moment running average coefficient
                 # In-place operations to update the averages at the same time
@@ -148,31 +145,21 @@ class LAdamW(Optimizer):
                 if p.dim() < 2:
                     exp_avg = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]).T / math.sqrt(rank),
                                            exp_avg)
-                    # exp_avg = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]).T, exp_avg)
                     exp_avg_sq = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]).T / math.sqrt(rank),
                                               exp_avg_sq)
-                    # exp_avg_sq = torch.matmul(projection[:rank * p.shape[0]].view(rank, p.shape[0]).T, exp_avg_sq)
                 elif p.dim() == 2:
                     if p.shape[0] >= p.shape[1]:
                         exp_avg = torch.matmul(
                             projection[:rank * p.shape[0]].view(rank, p.shape[0]).T / math.sqrt(rank), exp_avg)
-                        # exp_avg = torch.matmul(
-                        #     projection[:rank * p.shape[0]].view(rank, p.shape[0]).T, exp_avg)
                         exp_avg_sq = torch.matmul(
                             projection[:rank * p.shape[0]].view(rank, p.shape[0]).T / math.sqrt(rank), exp_avg_sq)
-                        # exp_avg_sq = torch.matmul(
-                        #     projection[:rank * p.shape[0]].view(rank, p.shape[0]).T, exp_avg_sq)
                     else:
                         exp_avg = torch.matmul(exp_avg,
                                                projection[:rank * p.shape[1]].view(p.shape[1], rank).T / math.sqrt(
                                                    rank))
-                        # exp_avg = torch.matmul(exp_avg,
-                        #                        projection[:rank * p.shape[1]].view(p.shape[1], rank).T)
                         exp_avg_sq = torch.matmul(exp_avg_sq,
                                                   projection[:rank * p.shape[1]].view(p.shape[1], rank).T / math.sqrt(
                                                       rank))
-                        # exp_avg_sq = torch.matmul(exp_avg_sq,
-                        #                           projection[:rank * p.shape[1]].view(p.shape[1], rank).T)
                 else:
                     raise ValueError("Parameters that exceed 2 Dim are not supported currently.")
 
@@ -181,10 +168,11 @@ class LAdamW(Optimizer):
                 correct_bias = group["correct_bias"]
                 step_size = lr
                 denom = exp_avg_sq.abs().sqrt().add_(eps)
+                step = self.state["step"]
                 if correct_bias:
-                    bias_correction1 = ((1.0 - beta1) * (1.0 - (beta0 * beta1) ** state["step"])) / (
+                    bias_correction1 = ((1.0 - beta1) * (1.0 - (beta0 * beta1) ** step)) / (
                             1.0 - (beta0 * beta1))
-                    bias_correction2 = ((1.0 - beta2) * (1.0 - (beta0 * beta2) ** state["step"])) / (
+                    bias_correction2 = ((1.0 - beta2) * (1.0 - (beta0 * beta2) ** step)) / (
                             1.0 - (beta0 * beta2))
                     step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
 

open_instruct/multipack.py
@@ -13,7 +13,6 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import transformers
-from accelerate import init_empty_weights
 from datasets import Dataset
 # from axolotl.monkeypatch.mixtral import patch_mixtral_moe_forward_zero3
 from torch.utils.data import BatchSampler, Sampler
@@ -411,9 +410,62 @@ def patch_for_multipack(model_type, model_name=None):
     #     )
 
 
+def patch_for_multipack_legacy(model_type, model_name=None):
+    # transformers.modeling_flash_attention_utils._get_unpad_data = get_unpad_data  # pylint: disable=protected-access
+    # if model_type == "mixtral":
+    #     transformers.models.mixtral.modeling_mixtral._get_unpad_data = (  # pylint: disable=protected-access
+    #         get_unpad_data
+    #     )
+    #     if is_deepspeed_zero3_enabled():
+    #         patch_mixtral_moe_forward_zero3()
+    if model_type == "llama":
+        transformers.models.llama.modeling_llama._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "qwen2":
+        transformers.models.qwen2.modeling_qwen2._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "qwen2_moe":
+        transformers.models.qwen2_moe.modeling_qwen2_moe._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "falcon":
+        transformers.models.falcon.modeling_falcon._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "phi":
+        transformers.models.phi.modeling_phi._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "gemma":
+        transformers.models.gemma.modeling_gemma._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "gemma2":
+        transformers.models.gemma2.modeling_gemma2._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "starcoder2":
+        transformers.models.starcoder2.modeling_starcoder2._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+    elif model_type == "gemmoe":
+        patch_remote(model_name, ".configuration_gemmoe", ".modeling_gemmoe")
+    elif model_type == "jamba":
+        patch_remote(model_name, ".configuration_jamba", ".modeling_jamba")
+    elif model_type == "deepseek_v2":
+        patch_remote(model_name, ".configuration_deepseek", ".modeling_deepseek")
+    elif model_type == "olmo":
+        transformers.models.olmo.modeling_olmo._get_unpad_data = (  # pylint: disable=protected-access
+            get_unpad_data
+        )
+
+
 def patch_remote(model_name, config_name, modeling_name):
     model_config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
     # we need to load the model here in order for modeling_* to be available
+    from accelerate import init_empty_weights
     with init_empty_weights():
         AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
     module_name = model_config.__class__.__module__.replace(config_name, modeling_name)