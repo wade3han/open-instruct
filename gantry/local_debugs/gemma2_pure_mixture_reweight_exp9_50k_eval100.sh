25b3a0a0b6e6800d523afdbbe5fcb60b370bbd61
Seungju Han <wade3han@gmail.com>
2024-08-16 17:19:01 +0900

    update

--- gantry/local_debugs/gemma2_pure_mixture_reweight_exp9_50k_eval100.sh
+++ gantry/local_debugs/gemma2_pure_mixture_reweight_exp9_50k_eval100.sh
@@ -9,24 +9,22 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # sweep learning rate from 2e-5 to 1e-6
 NAME=debugs
 
-export WANDB_ENTITY='seungjuhan3';
-export WANDB_PROJECT='lora_olmo1b_selections';
-export WANDB_NAME='reweighting-exp3-eval100';
-python open_instruct/gradient/finetune_pure_mixture_faster_exp3.py \
+export WANDB_ENTITY='seungjuhan3'
+export WANDB_PROJECT='lora_olmo1b_selections'
+export WANDB_NAME='gemma2_2b_reweighting-exp9-50k-eval100'
+python open_instruct/gradient/finetune_pure_mixture_faster_exp9.py \
   --use_multipack \
   --use_compile \
   --mask_users \
-  --model_name_or_path allenai/OLMo-1B-hf \
+  --model_name_or_path google/gemma-2-2b \
   --use_flash_attn \
-  --tokenizer_name allenai/OLMo-1B-hf \
+  --tokenizer_name google/gemma-2-2b \
   --train_file /net/nfs.cirrascale/mosaic/seungjuh/open-instruct/datasets/round0_data.jsonl \
   --max_seq_length 2048 \
   --preprocessing_num_workers 128 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
   --learning_rate 5e-5 \
-  --add_bos \
-  --use_slow_tokenizer False \
   --warmup_ratio 0.03 \
   --weight_decay 0. \
   --eval_per_steps 100 \
@@ -37,7 +35,7 @@ python open_instruct/gradient/finetune_pure_mixture_faster_exp3.py \
   --cooldown_ratio 0.2 \
   --logging_steps 1 \
   --clip_grad_norm 1.0 \
-  --max_train_samples 10000 \
+  --max_train_samples 50000 \
   --with_tracking \
   --report_to wandb \
   --reweighting \
