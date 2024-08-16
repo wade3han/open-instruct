07c988e59f0811107c1d3ceca2b2a8edbe83aa89
Seungju Han <wade3han@gmail.com>
2024-08-16 20:27:40 +0900

    tydiqa

--- open_instruct/gradient/collect_eval.py
+++ open_instruct/gradient/collect_eval.py
@@ -142,13 +142,90 @@ def get_bbh_dataset(data_dir: str):
     return dataset
 
 
+def get_tydiqa_dataset(data_dir: str, zh: bool = False):
+    """
+    Get the tydiqa dataset in the instruction tuning format. Each example is formatted as follows:
+
+    Query:
+    <|user|>
+    <Task Prompt>
+    <Passage>
+    <Question>
+    <|assistant|>
+    Answer:
+
+    Completion:
+    <Answer>
+
+    Args:
+        data_dir (str): The main data directory.
+        tokenizer (PreTrainedTokenizerBase): The tokenizer to use for tokenization.
+        max_length (int): The maximum length of the input sequence.
+        use_chat_format (bool, optional): Whether to use chat format. Defaults to True.
+        chat_format (str, optional): The chat format to use. Defaults to "tulu".
+        zh (bool, optional): Whether to use the Chinese validation examples. Defaults to False.
+
+    Returns:
+        Dataset: The tokenized TydiQA dataset.
+    """
+
+    # Same template as https://github.com/allenai/open-instruct/blob/main/eval/tydiqa/run_eval.py#L17
+    encoding_templates_with_context = {
+        "english": (
+            "Answer the following question based on the information in the given passage.", "Passage:", "Question:",
+            "Answer:"),
+        "arabic": ("أجب على السؤال التالي بناءً على المعلومات في المقطع المعطى.", "المقطع:", "السؤال:", "الإجابة:"),
+        "bengali": (
+            "প্রদত্ত অধ্যায়ের তথ্যের উপর ভিত্তি করে নিম্নলিখিত প্রশ্নের উত্তর দিন।", "অধ্যায়:", "প্রশ্ন:", "উত্তর:"),
+        "finnish": (
+            "Vastaa seuraavaan kysymykseen annetun kappaleen tiedon perusteella.", "Kappale:", "Kysymys:", "Vastaus:"),
+        "indonesian": (
+            "Jawab pertanyaan berikut berdasarkan informasi di bagian yang diberikan.", "Bagian:", "Pertanyaan:",
+            "Jawaban:"),
+        "korean": ("주어진 문단의 정보에 기반하여 다음 질문에 답하십시오.", "문단:", "질문:", "답변:"),
+        "russian": (
+            "Ответьте на следующий вопрос на основе информации в данном отрывке.", "Отрывок:", "Вопрос:", "Ответ:"),
+        "swahili": (
+            "Jibu swali lifuatalo kulingana na habari kwenye kifungu kilichotolewa.", "Kifungu:", "Swali:", "Jibu:"),
+        "telugu": ("ఇచ్చిన పేరాలోని సమాచారం ఆధారంగా కింది ప్రశ్నకు సమాధానం ఇవ్వండి.", "పేరా:", "ప్రశ్న:", "సమాధానం:")
+    }
+
+    # Chinese validation examples
+    if zh:
+        for lang in encoding_templates_with_context:
+            encoding_templates_with_context[lang] = (
+                "根据所给文章中的信息回答以下问题。", "文章:", "问题:", "答案:")
+
+    file_name = "tydiqa-one-shot-zh.json" if zh else "tydiqa-one-shot.json"
+    file = os.path.join(f"{data_dir}/eval/tydiqa", file_name)
+
+    examples = json.load(open(file, "r"))
+    dataset = []
+
+    for i, lang in enumerate(examples):
+        example = examples[lang][0]
+        prompt, p_template, q_template, a_template = encoding_templates_with_context[lang]
+        prompt += p_template + " " + \
+                  format(example["context"]) + "\n" + q_template + \
+                  " " + format(example["question"]) + "\n"
+        answer = " " + format(example["answers"][0]["text"])
+        dataset.append({"messages": [{"role": "user", "content": prompt}, {"role": "assistant", "content": answer}]})
+    return dataset
+
+
 if __name__ == "__main__":
     # data = get_mmlu_dataset("/net/nfs.cirrascale/mosaic/seungjuh/LESS/data")
     # with open("/net/nfs.cirrascale/mosaic/seungjuh/open-instruct-general/open_instruct/gradient/mmlu.jsonl", "w") as f:
     #     for example in data:
     #         f.write(json.dumps(example) + "\n")
 
-    data = get_bbh_dataset("/net/nfs.cirrascale/mosaic/seungjuh/LESS/data")
-    with open("/net/nfs.cirrascale/mosaic/seungjuh/open-instruct-general/open_instruct/gradient/bbh.jsonl", "w") as f:
+    # data = get_bbh_dataset("/net/nfs.cirrascale/mosaic/seungjuh/LESS/data")
+    # with open("/net/nfs.cirrascale/mosaic/seungjuh/open-instruct-general/open_instruct/gradient/bbh.jsonl", "w") as f:
+    #     for example in data:
+    #         f.write(json.dumps(example) + "\n")
+
+    data = get_tydiqa_dataset("/net/nfs.cirrascale/mosaic/seungjuh/LESS/data")
+    with open("/net/nfs.cirrascale/mosaic/seungjuh/open-instruct-general/open_instruct/gradient/tydiqa.jsonl",
+              "w") as f:
         for example in data:
             f.write(json.dumps(example) + "\n")
