5791769d2e03b3c8cd97d3017df20c4d43bcec52
Seungju Han <wade3han@gmail.com>
2024-08-16 11:18:43 +0900

    collect_eval

---
+++ open_instruct/gradient/collect_eval.py
@@ -0,0 +1,70 @@
+import os
+
+
+def get_mmlu_dataset(data_dir: str, ):
+    """
+    Get the MMLU dataset in the instruction tuning format. Each example is formatted as follows:
+
+    Query:
+    <|user|>
+    <Task Prompt>
+    <Question>
+    <|assistant|>
+    The answer is:
+
+    Completion:
+    <Answer>
+
+    Args:
+        data_dir (str): The main data directory.
+        tokenizer (Tokenizer): The tokenizer used to tokenize the input text.
+        max_length (int): The maximum length of the input sequence.
+        use_chat_format (bool, optional): Whether to use chat format for the prompts. Defaults to True.
+        chat_format (str, optional): The chat format to use for the prompts. Defaults to "tulu".
+
+    Returns:
+        Dataset: The tokenized dataset containing input_ids, attention_mask, and labels.
+    """
+    mmlu_data_dir = os.path.join(data_dir, "eval", "mmlu")
+    subjects = sorted(
+        [
+            f.split("_test.csv")[0]
+            for f in os.listdir(os.path.join(mmlu_data_dir, "test"))
+            if "_test.csv" in f
+        ]
+    )
+
+    def format_subject(subject):
+        l = subject.split("_")
+        s = ""
+        for entry in l:
+            s += " " + entry
+        return s
+
+    def gen_prompt(train_df, subject, i=0):
+        prompt = "The following are multiple choice questions (with answers) about {}.\n\n".format(
+            format_subject(subject)
+        )
+        prompt += format_example(train_df, i, include_answer=False)
+        return prompt
+
+    def format_example(df, idx, include_answer=True):
+        choices = ["A", "B", "C", "D"]
+        prompt = df.iloc[idx, 0]
+        k = df.shape[1] - 2
+        for j in range(k):
+            prompt += "\n{}. {}".format(choices[j], df.iloc[idx, j + 1])
+        prompt += "\nAnswer:"
+        return prompt
+
+    k = 5
+    dataset = {"messages": []}
+    for subject in subjects:
+        dev_df = pd.read_csv(
+            os.path.join(mmlu_data_dir, "dev", subject + "_dev.csv"), header=None
+        )[: k]
+        for i in range(k):
+            prompt = gen_prompt(dev_df, subject, i)
+            answer = " " + dev_df.iloc[i, dev_df.shape[1] - 2 + 1]
+            dataset["messages"].append([{"role": "user", "content": prompt}, {"role": "assistant", "content": answer}])
+    return dataset
