8d058e155ed2dac1048866a08d7e443494c1fd01
Seungju Han <wade3han@gmail.com>
2024-08-19 15:41:09 +0900

    fix: ip

--- open_instruct/gradient/utils.py
+++ open_instruct/gradient/utils.py
@@ -269,6 +269,106 @@ class GradientTrackerV2:
         return sim_matrix
 
 
+class GradientTrackerV2Ip:
+    def __init__(self, beta1: float, beta2: float, projector: CudaProjector, projector_batch_size: int, num_train: int):
+        self.beta1 = beta1
+        self.beta2 = beta2
+
+        self.num_train = num_train
+
+        self.train_gradient_store_exp_avg = {}
+        self.train_gradient_store_exp_avg_sq = {}
+        self.eval_gradient_store_avg = {}
+        self.count_per_dataset = {}
+        self.batch_size = projector_batch_size
+        self.projector = projector
+        self.previous_full_vectorized_grads = None
+
+        # store grads till the size becomes projector_batch_size.
+        self.temporary_gradient_storage = []
+        self.temporary_dataset_ids = []
+
+    def refresh(self):
+        self.temporary_gradient_storage = []
+        self.temporary_dataset_ids = []
+        self.previous_full_vectorized_grads = None
+
+    def train_reset(self):
+        self.train_gradient_store_exp_avg = {}
+        self.train_gradient_store_exp_avg_sq = {}
+        self.count_per_dataset = {}
+
+    def eval_reset(self):
+        self.eval_gradient_store_avg = {}
+
+    def track_gradients(self, model: nn.Module, dataset_id: int, valid_num_batches: int | None = None):
+        full_vectorized_grads = torch.cat(
+            [p.grad.view(-1) for n, p in model.named_parameters() if p.grad is not None])
+        if self.previous_full_vectorized_grads is None:
+            residual_full_vectorized_grads = full_vectorized_grads
+        else:
+            residual_full_vectorized_grads = full_vectorized_grads - self.previous_full_vectorized_grads
+        self.previous_full_vectorized_grads = full_vectorized_grads
+
+        self.temporary_gradient_storage.append(residual_full_vectorized_grads)
+        self.temporary_dataset_ids.append(dataset_id)
+
+        if len(self.temporary_gradient_storage) >= self.batch_size:
+            temporary_gradient_storage = torch.stack(self.temporary_gradient_storage, dim=0)
+            projected_vectorized_grads = self.projector.project(
+                temporary_gradient_storage.to(torch.float16).detach(),
+                model_id=0,
+            )
+
+            for i, dataset_id in enumerate(self.temporary_dataset_ids):
+                if self.count_per_dataset.get(dataset_id) is None:
+                    self.count_per_dataset[dataset_id] = 1
+                else:
+                    self.count_per_dataset[dataset_id] += 1
+
+                if valid_num_batches is not None:  # validation grads
+                    if self.eval_gradient_store_avg.get(dataset_id) is None:
+                        self.eval_gradient_store_avg[dataset_id] = projected_vectorized_grads[i]
+                    else:
+                        self.eval_gradient_store_avg[dataset_id] += projected_vectorized_grads[i] / valid_num_batches
+
+                else:
+                    if self.train_gradient_store_exp_avg.get(dataset_id) is None:
+                        self.train_gradient_store_exp_avg[dataset_id] = projected_vectorized_grads[i]
+                    else:
+                        self.train_gradient_store_exp_avg[dataset_id] = \
+                            self.beta1 * self.train_gradient_store_exp_avg[dataset_id] + \
+                            (1 - self.beta1) * projected_vectorized_grads[i]
+                    if self.train_gradient_store_exp_avg_sq.get(dataset_id) is None:
+                        self.train_gradient_store_exp_avg_sq[dataset_id] = projected_vectorized_grads[i] ** 2
+                    else:
+                        self.train_gradient_store_exp_avg_sq[dataset_id] = \
+                            self.beta2 * self.train_gradient_store_exp_avg_sq[dataset_id] + \
+                            (1 - self.beta2) * (projected_vectorized_grads[i] ** 2)
+
+            # reset temporary storage
+            self.temporary_gradient_storage = []
+            self.temporary_dataset_ids = []
+
+    def calc_sim(self, gradient_store_avg: dict[int, torch.tensor]):
+        num_train_set = self.num_train
+        num_valid_set = len(gradient_store_avg)
+
+        sim_matrix = torch.zeros((num_train_set, num_valid_set))
+        for train_id, val_id in itertools.product(range(num_train_set), range(num_valid_set)):
+            try:
+                train_grad = self.train_gradient_store_exp_avg[train_id] / torch.sqrt(
+                    self.train_gradient_store_exp_avg_sq[train_id] + 1e-8)
+                val_grad = gradient_store_avg[val_id]
+                # do inner product
+                sim = train_grad @ val_grad
+            except KeyError:
+                sim = -1
+            sim_matrix[train_id, val_id] = sim
+
+        return sim_matrix
+
+
 def initialize_optim_states(optimizer):
     for group in optimizer.param_groups:
         for p in group['params']:
