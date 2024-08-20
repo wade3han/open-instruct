8bf4ea93af92e826bfeff3bf01db7507e3ce1d39
Seungju Han <wade3han@gmail.com>
2024-08-20 17:56:43 +0900

    updates

--- gantry/reweight_expts/gemma2_gsm8k_late.sh
+++ gantry/reweight_expts/gemma2_gsm8k_late.sh
@@ -7,12 +7,12 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory,
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
-NAME='gemma2_2b_tydiqa_reweight'
+NAME=gemma2_2b_gsm8k_late_lr2e-5
 
 export WANDB_ENTITY='seungjuhan3'
 export WANDB_PROJECT='lora_olmo1b_selections'
 export WANDB_NAME=$NAME
-python open_instruct/gradient/finetune_reweight.py \
+python open_instruct/gradient/finetune_reweight_late_gsm8k.py \
   --use_multipack \
   --use_compile \
   --mask_users \
@@ -33,13 +33,13 @@ python open_instruct/gradient/finetune_reweight.py \
   --reduce_loss "sum" \
   --lr_scheduler_type "wsd" \
   --cooldown_ratio 0.2 \
-  --logging_steps 20\
+  --logging_steps 20 \
   --clip_grad_norm 1.0 \
   --max_train_samples 50000 \
   --with_tracking \
   --report_to wandb \
-  --reweighting \
-  --validation_dataset_names tydiqa \
-  --smoothing_factor 1e-3 \
+  --validation_dataset_names gsm8k \
   --lora_alpha 128 \
+  --max_test_samples 100 \
+  --checkpointing_steps 400 \
   --per_device_eval_batch_size 1
