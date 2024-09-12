c2a4f524797af7651bbd0a709d00193c603f7ded
Seungju Han <wade3han@gmail.com>
2024-09-12 22:50:04 +0900

    train with v0_2

--- scripts/fact_verifier/train_v0_2.sh
+++ scripts/fact_verifier/train_v0_2.sh
@@ -8,7 +8,7 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=v0_2_balanced_gemma2_seq3072;
+name=v0_2_1_balanced_gemma2_seq3072;
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
@@ -26,7 +26,7 @@ accelerate launch \
   --lora_dropout 0.05 \
   --tokenizer_name google/gemma-2-2b \
   --use_slow_tokenizer \
-  --train_file /home/ubuntu/scalable-factuality/adhoc/wiki/v0_2_balanced.jsonl \
+  --train_file /home/ubuntu/scalable-factuality/adhoc/wiki/v0_2_1_balanced.jsonl \
   --max_seq_length 3072 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
