dd952fb26fbb3360274f40c24fe890185311b14d
Seungju Han <wade3han@gmail.com>
2024-09-24 09:10:37 +0900

    update

--- scripts/fact_verifier/train_llama31_8b.sh
+++ scripts/fact_verifier/train_llama31_8b.sh
@@ -22,7 +22,7 @@ accelerate launch \
   --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
   --tokenizer_name meta-llama/Meta-Llama-3.1-8B-Instruct \
   --use_slow_tokenizer \
-  --train_file /home/ubuntu/build_open_instruct_three_hop_data_v0_2.jsonl \
+  --train_file /home/ubuntu/build_oi_no_reasoning.jsonl \
   --max_seq_length 3072 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
