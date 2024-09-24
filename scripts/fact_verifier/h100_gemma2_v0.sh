8df56da6a708df4a5744760d4f9334e8c8fb00dc
Seungju Han <wade3han@gmail.com>
2024-09-24 13:03:31 +0900

    fix

--- scripts/fact_verifier/h100_gemma2_v0.sh
+++ scripts/fact_verifier/h100_gemma2_v0.sh
@@ -37,5 +37,7 @@ accelerate launch \
   --num_train_epochs 1 \
   --output_dir $name \
   --report_to wandb \
+  --eval_file /home/ubuntu/open-instruct-general/eval.jsonl \
+  --eval_steps 1 \
   --logging_steps 10 \
   --with_tracking
