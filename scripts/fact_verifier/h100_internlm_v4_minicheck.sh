ddbd6d313df2f158d272cc0f16ba09213af22c29
Seungju Han <wade3han@gmail.com>
2024-09-27 14:11:40 +0900

    batch size adjust

--- scripts/fact_verifier/h100_internlm_v4_minicheck.sh
+++ scripts/fact_verifier/h100_internlm_v4_minicheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NUM_GPUS=4
-BATCH_SIZE_PER_GPU=1
+BATCH_SIZE_PER_GPU=2
 TOTAL_BATCH_SIZE=128
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE / $NUM_GPUS / $BATCH_SIZE_PER_GPU))
 echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
