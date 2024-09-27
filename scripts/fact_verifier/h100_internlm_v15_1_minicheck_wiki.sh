795edecc4dd0038c687c0d8bb4a9047b8b88f447
Seungju Han <wade3han@gmail.com>
2024-09-27 23:15:37 +0900

    update

--- scripts/fact_verifier/h100_internlm_v15_1_minicheck_wiki.sh
+++ scripts/fact_verifier/h100_internlm_v15_1_minicheck_wiki.sh
@@ -8,7 +8,7 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=internlm_v15_1_1e-6
+name=internlm_v15_1_minicheck_wiki
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
@@ -23,7 +23,7 @@ accelerate launch \
   --tokenizer_name internlm/internlm2_5-7b-chat \
   --trust_remote_code \
   --use_slow_tokenizer \
-  --train_file /home/ubuntu/v15_1.jsonl \
+  --train_file /home/ubuntu/v15_1_minicheck_wiki.jsonl \
   --max_seq_length 2048 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
