fd345971483f7848605a3df4425bd9d6a44e0fac
Seungju Han <wade3han@gmail.com>
2024-10-03 15:18:19 +0900

    update

--- scripts/fact_verifier/llama32_1b/base_v15_1_anli.sh
+++ scripts/fact_verifier/llama32_1b/base_v15_1_anli.sh
@@ -8,7 +8,7 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=llama3_2_1B_v15_1_anli
+name=base_llama3_2_1B_v15_1_anli
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
