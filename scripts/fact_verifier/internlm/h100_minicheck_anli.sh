55c276ddd7d6fcc4e4a3b54b70c41ad9c3f28a69
Seungju Han <wade3han@gmail.com>
2024-10-08 11:00:43 +0900

    updte

--- scripts/fact_verifier/internlm/h100_minicheck_anli.sh
+++ scripts/fact_verifier/internlm/h100_minicheck_anli.sh
@@ -8,7 +8,7 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=internlm_minicheck_anli_lr1e-6
+name=internlm_minicheck_anli_lr5e-7
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
@@ -31,7 +31,7 @@ accelerate launch \
   --lr_scheduler_type linear \
   --warmup_ratio 0.03 \
   --weight_decay 0. \
-  --num_train_epochs 2 \
+  --num_train_epochs 1 \
   --output_dir $name \
   --report_to wandb \
   --eval_file /home/ubuntu/open-instruct-general/fact_verification_dev.jsonl \
