cf05be4026b705e4692e073a9d1e4b9d5204a053
Seungju Han <wade3han@gmail.com>
2024-10-09 17:11:37 +0900

    fix

--- scripts/fact_verifier/llama32_1b_controlled/v20_noise_anli_different.sh
+++ scripts/fact_verifier/llama32_1b_controlled/v20_noise_anli_different.sh
@@ -8,12 +8,12 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=llama3_2_1B_v20_noise_anli_different
+name=llama3_2_1B_v20_noise_anli_different_fix
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
   --num_processes $NUM_GPUS \
-  --main_process_port 29509 \
+  --main_process_port 29510 \
   --use_deepspeed \
   --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
   open_instruct/finetune.py \
