10c6f3a9e180e78ce0c2764f4b040c788af67a74
Seungju Han <wade3han@gmail.com>
2024-10-12 06:05:41 +0900

    update

--- scripts/fact_verifier/llama32_1b/v20_v21_anli_full_wanli_full.sh
+++ scripts/fact_verifier/llama32_1b/v20_v21_anli_full_wanli_full.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
-NUM_GPUS=4
-BATCH_SIZE_PER_GPU=1
-TOTAL_BATCH_SIZE=4
+NUM_GPUS=1
+BATCH_SIZE_PER_GPU=4
+TOTAL_BATCH_SIZE=128
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE / $NUM_GPUS / $BATCH_SIZE_PER_GPU))
 echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 # You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory,
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=internlm_v20_v21_anli_minicheck_bsz4
+name=llama3_2_1B_v20_v21_anli_full_wanli_full
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
   --num_processes $NUM_GPUS \
   --use_deepspeed \
+  --main_process_port 29512 \
   --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
   open_instruct/finetune.py \
   --wandb_entity seungjuhan3 \
-  --wandb_project fact_verifier \
+  --wandb_project fact_verifier_controlled \
   --wandb_name $name \
-  --model_name_or_path internlm/internlm2_5-7b-chat \
-  --tokenizer_name internlm/internlm2_5-7b-chat \
-  --trust_remote_code \
+  --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
+  --use_lora \
+  --lora_rank 64 \
+  --lora_alpha 16 \
+  --lora_dropout 0.05 \
+  --tokenizer_name meta-llama/Llama-3.2-1B-Instruct \
   --use_slow_tokenizer \
-  --train_file /home/ubuntu/v20_v21_anli_minicheck.jsonl \
+  --train_file /home/ubuntu/v20_v21_anli_full_wanli_full.jsonl \
   --max_seq_length 2048 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-  --learning_rate 1e-6 \
+  --learning_rate 5e-5 \
   --lr_scheduler_type linear \
   --warmup_ratio 0.03 \
   --weight_decay 0. \
@@ -35,7 +39,6 @@ accelerate launch \
   --output_dir $name \
   --report_to wandb \
   --eval_file /home/ubuntu/open-instruct-general/fact_verification_dev.jsonl \
-  --eval_steps 800 \
-  --gradient_checkpointing \
+  --eval_steps 40 \
   --logging_steps 10 \
   --with_tracking
