31ba3ea6d1d7222d1dcfdf0a38c1a6d5e224ae61
Seungju Han <wade3han@gmail.com>
2024-10-15 14:04:18 +0900

    fix

--- scripts/fact_verifier/internlm/v15_1_decompose_and_verify.sh
+++ scripts/fact_verifier/internlm/v15_1_decompose_and_verify.sh
@@ -23,7 +23,7 @@ accelerate launch \
   --tokenizer_name internlm/internlm2_5-7b-chat \
   --trust_remote_code \
   --use_slow_tokenizer \
-  --train_file /home/ubuntu/train_decompose_then_verify_v15_1.jsonl.jsonl \
+  --train_file /home/ubuntu/train_decompose_then_verify_v15_1.jsonl \
   --max_seq_length 2048 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
