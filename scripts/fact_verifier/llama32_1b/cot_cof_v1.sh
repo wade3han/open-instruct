8f8847f1b5027a92c0e220d9926fdd6bc00ce09e
Seungju Han <wade3han@gmail.com>
2024-10-21 15:19:15 +0900

    fix

--- scripts/fact_verifier/llama32_1b/cot_cof_v1.sh
+++ scripts/fact_verifier/llama32_1b/cot_cof_v1.sh
@@ -27,7 +27,7 @@ accelerate launch \
   --lora_dropout 0.05 \
   --tokenizer_name meta-llama/Llama-3.2-1B-Instruct \
   --use_slow_tokenizer \
-  --train_file /home/ubuntu/v1_messages_no_cot.jsonl \
+  --train_file /home/ubuntu/v1_messages_cot.jsonl \
   --max_seq_length 2048 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
