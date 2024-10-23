90b8ad2657bcba8992b6a634e7f3510f51246539
Seungju Han <wade3han@gmail.com>
2024-10-23 19:37:12 +0900

    update

--- scripts/fact_verifier/llama32_1b/v3_7.sh
+++ scripts/fact_verifier/llama32_1b/v3_7.sh
@@ -14,7 +14,7 @@ accelerate launch \
   --num_machines 1 \
   --num_processes $NUM_GPUS \
   --use_deepspeed \
-  --main_process_port 29505 \
+  --main_process_port 29508 \
   --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
   open_instruct/finetune.py \
   --wandb_entity seungjuhan3 \
