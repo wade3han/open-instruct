521789b8890a036bbd07a7dbdb4bedfd5a32d8fe
Seungju Han <wade3han@gmail.com>
2024-10-23 17:55:36 +0900

    update

--- scripts/fact_verifier/llama32_1b/v3_5.sh
+++ scripts/fact_verifier/llama32_1b/v3_5.sh
@@ -8,13 +8,13 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=llama3_2_1B_v3_5_1eps
+name=llama3_2_1B_v3_5_2eps
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
   --num_processes $NUM_GPUS \
   --use_deepspeed \
-  --main_process_port 29503 \
+  --main_process_port 29505 \
   --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
   open_instruct/finetune.py \
   --wandb_entity seungjuhan3 \
@@ -35,7 +35,7 @@ accelerate launch \
   --lr_scheduler_type linear \
   --warmup_ratio 0.03 \
   --weight_decay 0. \
-  --num_train_epochs 1 \
+  --num_train_epochs 2 \
   --output_dir $name \
   --report_to wandb \
   --eval_file /home/ubuntu/open-instruct-general/fact_verification_dev.jsonl \
