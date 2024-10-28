f78ac49db1a1da728d67707d4aa52a0653599045
Seungju Han <wade3han@gmail.com>
2024-10-28 14:57:19 +0900

    fix

--- scripts/fact_verifier/llama32_1b/v4_anli.sh
+++ scripts/fact_verifier/llama32_1b/v4_anli.sh
@@ -27,7 +27,7 @@ accelerate launch \
   --lora_dropout 0.05 \
   --tokenizer_name meta-llama/Llama-3.2-1B-Instruct \
   --use_slow_tokenizer \
-  --train_file /home/ubuntu/v4-anli.jsonl
+  --train_file /home/ubuntu/v4-anli.jsonl \
   --max_seq_length 2048 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
