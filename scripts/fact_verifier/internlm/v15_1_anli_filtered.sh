2ce7702e50ba07892cf4818b865d3480cd640e73
Seungju Han <wade3han@gmail.com>
2024-10-30 22:29:22 +0900

    update

---
+++ scripts/fact_verifier/internlm/v15_1_anli_filtered.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+NUM_GPUS=1
+BATCH_SIZE_PER_GPU=2
+TOTAL_BATCH_SIZE=4
+GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE / $NUM_GPUS / $BATCH_SIZE_PER_GPU))
+echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+# You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory,
+# but it will trade off speed.
+# sweep learning rate from 2e-5 to 1e-6
+
+name=internlm_7B_v15_1_anli_filtered_2eps
+accelerate launch \
+  --mixed_precision bf16 \
+  --num_machines 1 \
+  --num_processes $NUM_GPUS \
+  --use_deepspeed \
+  --main_process_port 29514 \
+  --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
+  open_instruct/finetune.py \
+  --wandb_entity seungjuhan3 \
+  --wandb_project fact_verifier_controlled \
+  --wandb_name $name \
+  --model_name_or_path internlm/internlm2_5-7b-chat \
+  --tokenizer_name internlm/internlm2_5-7b-chat \
+  --use_slow_tokenizer \
+  --trust_remote_code \
+  --use_lora \
+  --lora_rank 64 \
+  --lora_alpha 16 \
+  --lora_dropout 0.05 \
+  --train_file /home/ubuntu/v15_1-anli-filtered.jsonl \
+  --max_seq_length 1024 \
+  --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+  --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+  --learning_rate 5e-5 \
+  --lr_scheduler_type linear \
+  --warmup_ratio 0.03 \
+  --weight_decay 0. \
+  --num_train_epochs 2 \
+  --output_dir $name \
+  --report_to wandb \
+  --eval_file /home/ubuntu/open-instruct-general/fact_verification_dev.jsonl \
+  --eval_steps 10000 \
+  --gradient_checkpointing \
+  --logging_steps 10 \
+  --with_tracking
