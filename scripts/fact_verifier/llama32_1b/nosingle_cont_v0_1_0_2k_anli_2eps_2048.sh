00d653e51887304f41d321f819221d98088742f3
Seungju Han <wade3han@gmail.com>
2024-11-11 19:40:42 +0900

    update

--- scripts/fact_verifier/llama32_1b/nosingle_cont_v0_1_0_2k_anli_2eps_2048.sh
+++ scripts/fact_verifier/llama32_1b/nosingle_cont_v0_1_0_2k_anli_2eps_2048.sh
@@ -8,7 +8,7 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
 
-name=llama32_1B_cont_v2_0_0_anli_2eps_2048
+name=llama32_1B_nosingle_cont_v0_1_0_2k_anli_2eps_2048
 accelerate launch \
   --mixed_precision bf16 \
   --num_machines 1 \
@@ -27,7 +27,7 @@ accelerate launch \
   --lora_rank 64 \
   --lora_alpha 16 \
   --lora_dropout 0.05 \
-  --train_file /home/ubuntu/scalable-factuality/train/train/cont_v2_0_0-anli.jsonl \
+  --train_file /home/ubuntu/scalable-factuality/train/train/size_nosingle_cont_v0_1_0_2000-anli.jsonl \
   --max_seq_length 2048 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
