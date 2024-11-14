f04289c7c87b3ce14bb9da220da7e33031c31abb
Seungju Han <wade3han@gmail.com>
2024-11-14 23:56:55 +0900

    update

--- scripts/fact_verifier/llama31_8b/cont_v3_0_1_8k_anli_21k_c2d.sh
+++ scripts/fact_verifier/llama31_8b/cont_v3_0_1_8k_anli_21k_c2d.sh
@@ -27,7 +27,7 @@ accelerate launch \
   --lora_rank 64 \
   --lora_alpha 16 \
   --lora_dropout 0.05 \
-  --train_file /home/ubuntu/scalable-factuality/train/train/size_cont_v3_0_1_8000-size_anli-c2d.jsonl \
+  --train_file /home/ubuntu/scalable-factuality/train/train/size_cont_v3_0_1_8000-anli-c2d.jsonl \
   --max_seq_length 2048 \
   --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
   --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
