465c4aa2be5575273d2403c9fbc10d85904baf66
Seungju Han <wade3han@gmail.com>
2024-11-22 19:37:31 +0900

    update

---
+++ scripts/fact_verifier/llama32_1b/mt_cot_8_stage2_rft3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+NUM_GPUS=1
+BATCH_SIZE_PER_GPU=4
+TOTAL_BATCH_SIZE=4
+GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE / $NUM_GPUS / $BATCH_SIZE_PER_GPU))
+echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+# You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory,
+# but it will trade off speed.
+# sweep learning rate from 2e-5 to 1e-6
+
+name=llama32_1B_dv_stepbystepv2_2eps_stage2_rft3
+accelerate launch \
+  --mixed_precision bf16 \
+  --num_machines 1 \
+  --num_processes $NUM_GPUS \
+  --use_deepspeed \
+  --main_process_port 29503 \
+  --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
+  open_instruct/finetune.py \
+  --seed 1 \
+  --wandb_entity seungjuhan3 \
+  --wandb_project fact_verifier_controlled \
+  --wandb_name $name \
+  --model_name_or_path /home/ubuntu/open-instruct-general/llama32_1B_dv_stepbystepv2_2eps/temp \
+  --tokenizer_name /home/ubuntu/open-instruct-general/llama32_1B_dv_stepbystepv2_2eps/temp \
+  --use_slow_tokenizer \
+  --use_lora \
+  --lora_rank 64 \
+  --lora_alpha 128 \
+  --lora_dropout 0.05 \
+  --train_file /home/ubuntu/scalable-factuality/train/train/cont_v3_0_1_train_step2-llama32_1B_dv_stepbystepv2_2eps-anli_train_step2-llama32_1B_dv_stepbystepv2_2eps.jsonl,/home/ubuntu/scalable-factuality/train/train_cot/cont_v3_0_1_train_step2-llama32_1B_dv_stepbystepv2_2eps-anli_train_step2-llama32_1B_dv_stepbystepv2_2eps.jsonl \
+  --max_seq_length 2048 \
+  --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+  --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+  --learning_rate 5e-5 \
+  --lr_scheduler_type linear \
+  --warmup_ratio 0.03 \
+  --weight_decay 0. \
+  --num_train_epochs 2 \
+  --output_dir $name \
+  --report_to wandb \
+  --eval_file /home/ubuntu/open-instruct-general/fact_verification_dev.jsonl \
+  --eval_steps 10000 \
+  --logging_steps 10 \
+  --with_tracking
